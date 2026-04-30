2e536cf4f819b7e6ae1cdcac269359cb249dcf9e
epf-21 <efrainperalta.f@gmail.com>
2026-04-30 13:30:27 -0400

    ajustar imagen de guia

--- components/AddAccountModal.tsx
+++ components/AddAccountModal.tsx
@@ -243,7 +243,7 @@ const styles = StyleSheet.create({
     },
     guideImage: {
         width: '100%',
-        height: 420,
+        height: 225,
         borderRadius: 12,
         backgroundColor: '#F4F7F8',
     },
